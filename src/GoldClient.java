3787b14950145b36b7b24668677f88a4acbea1f1
Ismailaboamra <ismail.amra12@gmail.com>
2024-01-17 09:02:49 +0200

    GoldClient Class- Part2.

---
+++ src/GoldClient.java
@@ -0,0 +1,11 @@
+public class GoldClient extends Client{
+
+    public GoldClient(int idClient, String lastName, String firstName) {
+        super(idClient, lastName, firstName, 0.02, 0.003);
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
