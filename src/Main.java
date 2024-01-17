112387d5a63840fdd5a0c22d2d1a74985560697b
Ismailaboamra <ismail.amra12@gmail.com>
2024-01-17 08:59:44 +0200

    Main- Part2.

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,81 @@
 
 public class Main {
     public static void main(String[] args) {
-        Log l1 = new Log(123,"Money Deposit",2000);
-        Log l2 = new Log(456,"Money Withdraw",1500);
+        Log l1 = new Log(123, "Money Deposit", 2000);
+        Log l2 = new Log(456, "Money Withdraw", 1500);
+        Account a1 = new Account(1001, 1000);
+        Account a2 = new Account(1002, 3000);
+        Account a3 = new Account(1003, 2500);
+        Account a4 = new Account(1004, 10_000);
+        Account a5 = new Account(1005, 6000);
+        Account a6 = new Account(1006, 1000);
+        Account a7 = new Account(1007, 3000);
+        Account a8 = new Account(1008, 2500);
+        Account a9 = new Account(1009, 10_000);
+        Account a10 = new Account(1010, 6000);
+        Account a11 = new Account(1011, 1000);
+        Account a12 = new Account(1012, 3000);
+        Account a13 = new Account(1013, 2500);
+        Account a14 = new Account(1014, 10_000);
+        Account a15 = new Account(1015, 6000);
+        Account a16 = new Account(1016, 1000);
+        Account a17 = new Account(1017, 3000);
+        Account a18 = new Account(1018, 2500);
+        Account a19 = new Account(1019, 10_000);
+        Account a20 = new Account(1020, 6000);
+        Account a21 = new Account(1021, 1000);
+        Account a22 = new Account(1022, 3000);
+        Account a23 = new Account(1023, 2500);
+        Account a24 = new Account(1024, 10_000);
+        Account a25 = new Account(1025, 6000);
 
-        Account a1 = new Account(111, 1000);
-        Account a2 = new Account(222, 3000);
-        Account a3 = new Account(333, 2500);
-        Account a4 = new Account(444, 10_000);
-        Account a5 = new Account(555, 6000);
-
-
-
-
-        System.out.println(l1);
-        System.out.println(l2);
+        Account a26 = new Account(1026, 1000);
+        Account a27 = new Account(1027, 3000);
+        Account a28 = new Account(1028, 2500);
+        Account a29 = new Account(1029, 10_000);
+        Account a30 = new Account(1030, 6000);
+        Account a31 = new Account(1031, 1000);
+        Account a32 = new Account(1032, 3000);
+        Account a33 = new Account(1033, 2500);
+        Account a34 = new Account(1034, 10_000);
+        Account a35 = new Account(1035, 6000);
+        Account a36 = new Account(1036, 1000);
+        Account a37 = new Account(1037, 3000);
+        Account a38 = new Account(1038, 2500);
+        Account a39 = new Account(1039, 10_000);
+        Account a40 = new Account(1040, 6000);
+        Account a41 = new Account(1041, 1000);
+        Account a42 = new Account(1042, 3000);
+        Account a43 = new Account(1043, 2500);
+        Account a44 = new Account(1044, 10_000);
+        Account a45 = new Account(1045, 6000);
 
         Logger logger = Logger.getInstance();
         logger.addLog(l1);
         logger.addLog(l2);
         logger.display();
 
-        System.out.println(a1);
-        System.out.println(a2);
-        System.out.println(a3);
-        System.out.println(a4);
-        System.out.println(a5);
-
-
-
-
-
-
+        Client regular1 = new RegularClient(111, "Cohen", "Avi");
+        regular1.addAccount(a1);
+        regular1.addAccount(a2);
+        regular1.addAccount(a3);
+        regular1.addAccount(a4);
+        regular1.addAccount(a5);
+        regular1.fullReport();
+        Client regular2 = new RegularClient(112, "Lombok", "Yakov");
+        regular2.addAccount(a6);
+        regular2.addAccount(a7);
+        regular2.addAccount(a8);
+        regular2.addAccount(a9);
+        regular2.addAccount(a10);
+        regular2.fullReport();
+        Client regular3 = new RegularClient(113, "Tomcat", "Tommy");
+        regular3.addAccount(a11);
+        regular3.addAccount(a12);
+        regular3.addAccount(a13);
+        regular3.addAccount(a14);
+        regular3.addAccount(a15);
+        regular3.fullReport();
 
 
     }
