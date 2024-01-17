7f34ad13926737a2a31be9888ead1367c999f03f
Ismailaboamra <ismail.amra12@gmail.com>
2024-01-17 09:28:27 +0200

    PlatinumClient Class implement fullReport() & toString() function- Part2.

--- src/PlatinumClient.java
+++ src/PlatinumClient.java
@@ -3,6 +3,31 @@ public class PlatinumClient extends Client{
         super(idClient, lastName, firstName, 0.01, 0.005);
     }
 
+    public void fullReport()
+    {
+        System.out.println("------------------------------------------------");
+        System.out.println("Full Report About the Client :");
+        System.out.println("Client ID : " + getIdClient());
+        System.out.println("Full Name : " + getFullName());
+        System.out.println("Commission Rate : " + getCommissionRate());
+        System.out.println("Interest Rate : " + getInterestRate());
+        System.out.println("Client Type : PlatinumClient");
+        System.out.println("------------------------------------------------");
+
+    }
+
+    @Override
+    public String toString() {
+        return "PlatinumClient{" +
+                "idClient=" + super.getIdClient() +
+                ", lastName='" + super.getLastName() + '\'' +
+                ", firstName='" + super.getFirstName() + '\'' +
+                ", accounts=" + super.accounts_str() +
+                ", commissionRate=" + getCommissionRate() +
+                ", interestRate=" + getInterestRate() +
+                '}';
+    }
+
     @Override
     public void draw() {
         System.out.println("  #####  #######  #####  ######");
